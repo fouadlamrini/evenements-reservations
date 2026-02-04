8c55843c72a88a25d2ec9bf26d19e63451268846
fouad lmrini <fouad11.lamrini@gmail.com>
2026-02-04 12:06:52 +0100

    RE-9 jwtstrategy+jwtAuthGuard+userServce

--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument, Role } from './schemas/user.schema';
+import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async create(createUserDto: CreateUserDto, role: Role): Promise<User> {
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+    const createdUser = new this.userModel({
+      ...createUserDto,
+      password: hashedPassword,
+      role,
+    });
+    return createdUser.save();
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.userModel.findOne({ email });
+  }
+}
