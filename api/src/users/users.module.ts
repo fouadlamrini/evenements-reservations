763af9efb1ac8c896d1b9ecdd5a63acf48f8d67e
fouad lmrini <fouad11.lamrini@gmail.com>
2026-02-04 12:25:27 +0100

    RE-138 (done) create admin par seeder

--- api/src/users/users.module.ts
+++ api/src/users/users.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schemas/user.schema';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-
+import { UsersSeeder } from './seeder/users.seeder';
 @Module({
-  providers: [UsersService],
-  controllers: [UsersController]
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  providers: [UsersService, UsersSeeder],
+  exports: [UsersService],
+  controllers: [UsersController],
 })
 export class UsersModule {}
--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User, UserDocument, Role } from './schemas/user.schema';
+import { User, UserDocument } from './schemas/user.schema';
+import { Role } from '../roles/role.enum';
 import { CreateUserDto } from './dto/create-user.dto';
 import * as bcrypt from 'bcrypt';
 
